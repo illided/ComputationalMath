c8080a3442f1e5bd2111713e2d2241e31f74b9b3
ilya <utkin.ilya.nickolaevich@gmail.com>
2021-11-02 11:59:42 +0300

    bisection method fix

--- hw1/clarification_methods/bisection_method.py
+++ hw1/clarification_methods/bisection_method.py
@@ -11,8 +11,20 @@ class BisectionMethod(ClarificationMethod, ABC):
         a = self.segment[0]
         b = self.segment[1]
         middle = a + (b - a) / 2
-        if self.expr(a) * self.expr(middle) <= 0:
+        f_a = self.expr(a) - self.target
+        f_b = self.expr(b) - self.target
+        f_m = self.expr(middle) - self.target
+        if f_m == 0:
+            self.current_x = middle
+            return
+        if f_a == 0:
+            self.current_x = a
+        if f_b == 0:
+            self.current_x = b
+        if f_a * f_m < 0:
             self.segment = [a, middle]
-        else:
+        elif f_b * f_m < 0:
             self.segment = [middle, b]
+        else:
+            raise ValueError("Segmentation error")
         self.current_x = middle
